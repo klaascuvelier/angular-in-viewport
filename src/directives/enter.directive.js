4da5a93b1b45c6f67a3a969a0acfc997ded12eb0
Klaas Cuvelier <cuvelierklaas@gmail.com>
2019-01-28 14:43:31 +0100

    Move leave-callback outside of scope apply

--- src/directives/enter.directive.js
+++ src/directives/enter.directive.js
@@ -29,18 +29,19 @@
         if (iElement[0].nodeType !== 8 && iAttrs.viewportEnter) {
             controller.add('enter', iElement[0], function () {
                 $scope.$apply(function () {
-                    if (iAttrs.viewportLeave && iElement.attr('viewport-leave-registered') !== 'true') {
-                        iElement.attr('viewport-leave-registered', 'true');
-
-                        // Lazy add leave callback
-                        controller.add('leave', iElement[0], function () {
-                            $scope.$apply(function () {
-                                $scope.$eval(iAttrs.viewportLeave);
-                            });
-                        });
-                    }
                     $scope.$eval(iAttrs.viewportEnter);
                 });
+
+                if (iAttrs.viewportLeave && iElement.attr('viewport-leave-registered') !== 'true') {
+                    iElement.attr('viewport-leave-registered', 'true');
+
+                    // Lazy add leave callback
+                    controller.add('leave', iElement[0], function () {
+                        $scope.$apply(function () {
+                            $scope.$eval(iAttrs.viewportLeave);
+                        });
+                    });
+                }
             });
         }
     }
